616dec76e87509316a84c802d0497e2ffd7802bb
daochinam <daochinam1081406@gmail.com>
2023-11-26 11:07:23 +0700

    create pj

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Booking_Ticket_Movie from "./Booking_Ticket_Movie/Booking_Ticket_Movie";
+
+function App() {
+  return <Booking_Ticket_Movie />;
+}
+
+export default App;
---
+++ src/Booking_Ticket_Movie/Booking_Ticket_Movie.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Booking_Ticket_Movie() {
+  return <div>Booking_Ticket_Movie</div>;
+}
